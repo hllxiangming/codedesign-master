7515cfb0c18494422fb3245651f10066ec5435be
hll <huanglili6088@163.com>
2021-02-23 00:24:41 +0800

    Initial commit
    add codedesign 20210223

---
+++ com/mycompany/mypro/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.mycompany.mypro;
+
+/**
+* @author  hll
+* @version 1.0
+*/
+public class HelloWorld{
+public HelloWorld(){}
+public static void main(String[] args)
+{
+  System.out.println("Hello World!");
+	
+}
+}
